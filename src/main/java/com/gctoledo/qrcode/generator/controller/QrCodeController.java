65a8659a89ace078f143e4d93082b793558f7368
Gabriel <gabrielcostatoledo@outlook.com>
2025-04-25 11:36:27 -0300

    feat: implements generate QrCode method

--- src/main/java/com/gctoledo/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/gctoledo/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.gctoledo.qrcode.generator.controller;
 
 import com.gctoledo.qrcode.generator.controller.dto.qrcode.QrCodeGenerateRequest;
 import com.gctoledo.qrcode.generator.controller.dto.qrcode.QrCodeGenerateResponse;
+import com.gctoledo.qrcode.generator.service.QrCodeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeService qrCodeService;
+
+    public QrCodeController(QrCodeService qrCodeService) {
+        this.qrCodeService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generateQrCode(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeService.generateQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
